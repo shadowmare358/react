317dfe37b5b6d3932eb3b17d0de63f98151e3b03
Mike <shadowmare@windowslive.com>
2019-04-23 11:04:25 +0200

    Updates

---
+++ precache-manifest.8932fd7b4ee88a718d9935c1c2d8b18b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c42a1d749ea5f0817e8e39271835b0",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "950e1668d3066145845a",
+    "url": "/react/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "950e1668d3066145845a",
+    "url": "/react/static/js/main.91eb3126.chunk.js"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react/static/media/logo.ee7cd8ed.svg"
+  }
+]);
